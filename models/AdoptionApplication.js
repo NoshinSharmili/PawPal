9c47d8fa3fa3188291277020aa7f06f8e91cd6ca
fatiha693 <167349551+fatiha693@users.noreply.github.com>
2025-07-16 12:41:17 +0600

    Add files via upload
    adoption application

---
+++ models/AdoptionApplication.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const adoptionApplicationSchema = new mongoose.Schema({
+  petId: { type: mongoose.Schema.Types.ObjectId, ref: 'Pet', required: true },
+  fullName: { type: String, required: true },
+  profession: { type: String, required: true },
+  email: { type: String, required: true },
+  address: { type: String, required: true },
+  isHousePetProofed: { type: Boolean, required: true },
+  familyInformation: { type: String, required: true },
+  nidNumber: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  reasonToAdopt: { type: String, required: true },
+  submittedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('AdoptionApplication', adoptionApplicationSchema); 
