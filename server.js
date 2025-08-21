05db38d250595fa5ee361988d0553452a827c544
fatiha693 <fatiha@iut-dhaka.edu>
2025-08-22 02:07:26 +0600

    added foster system

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ const userRoutes = require('./routes/userRoutes');
 const petRoutes = require('./routes/petRoutes');
 const adoptionApplicationRoutes = require('./routes/adoptionApplicationRoutes');
 const vetRoutes = require('./routes/vetRoutes');
-<<<<<<< HEAD
 const fosterRoutes = require('./routes/fosterRoutes'); // NEW: Foster routes
-=======
->>>>>>> e1201dbb78164aa3549184cd08020bd0332f1f98
 const cors = require('cors');
 
 dotenv.config();
@@ -23,10 +20,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/pets', petRoutes);
 app.use('/api/adoption-applications', adoptionApplicationRoutes);
 app.use('/api/vets', vetRoutes);
-<<<<<<< HEAD
 app.use('/api/fosters', fosterRoutes); // NEW: Foster routes
 
-=======
->>>>>>> e1201dbb78164aa3549184cd08020bd0332f1f98
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
